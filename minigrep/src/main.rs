71d70ea76da969f7c175569390f37dc39ccdb1d7
USER <user@USERui-MacBookPro.local>
2020-01-28 00:31:51 +0900

    Cp.12 Finished

---
+++ minigrep/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("인수 분석 중 오류 발생: {}",err);
+        process::exit(1);
+    });
+
+    println!("query: {}, targetfile: {}", config.query, config.filename);
+
+    // run(config);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("app err: {}", e);
+        process::exit(1);
+    }
+
+}
