a0f654be6b8b4323c13257da70332523d9efd9d7
yonggyun-park <yonggyun.park@navercorp.com>
2020-05-26 10:04:27 +0900

    add cp. 17

---
+++ blog_ref/src/main.rs
@@ -0,0 +1,15 @@
+extern crate blog_ref;
+use blog_ref::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
